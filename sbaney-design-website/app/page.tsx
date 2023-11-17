60942a39403752dac20827ecae4cfe3d8e809a34
Steve Baney <steve.baney@gmail.com>
2023-11-17 13:40:16 -0500

    Implemented nav by moving functionality from /app/page.tsx to /app/layout.tsx children:React.ReactNode section

--- sbaney-design-website/app/page.tsx
+++ sbaney-design-website/app/page.tsx
@@ -2,11 +2,15 @@ import MainPageNav from "@/components/MainPageNav";
 import getMainPagesMetadata from "@/components/getMainPageMetadata";
 
 const HomePage = () => {
-  const mainPageMetadata = getMainPagesMetadata();
-  const mainPageNav = mainPageMetadata.map((mainPage) => (
-    <MainPageNav key={mainPage.slug} {...mainPage} />
-  ));
-  return <div>{mainPageNav}</div>;
+  //const mainPageMetadata = getMainPagesMetadata();
+  //const mainPageNav = mainPageMetadata.map((mainPage) => (
+  //  <MainPageNav key={mainPage.slug} {...mainPage} />
+  //));
+  //return (
+  //  <nav>
+  //    <div className="flex items-center gap-4">{mainPageNav}</div>
+  //  </nav>
+  //);
 };
 
 export default HomePage;
