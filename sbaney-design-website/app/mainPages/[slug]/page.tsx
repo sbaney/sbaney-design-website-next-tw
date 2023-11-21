ae065b57e597245ca5092a5dfe88306a2a0eea77
Steve Baney <steve.baney@gmail.com>
2023-11-21 12:45:49 -0500

    everything is working?

--- sbaney-design-website/app/mainPages/[slug]/page.tsx
+++ sbaney-design-website/app/mainPages/[slug]/page.tsx
@@ -24,32 +24,10 @@ const mainPage = (props: any) => {
   const mainPage = getMainPageContent(slug);
   return (
     <div className="">
-      <div className="grid grid-cols-10">
-        <div className="col-span-9">
-          <h1>This is a main page: {mainPage.data.title}</h1>
-          <article className="prose">
-            <Markdown>{mainPage.content}</Markdown>
-          </article>
-        </div>
-        <div className="col-span-1 md:hidden overflow-y-auto">
-          <div className="fixed bg-sky-500 text-red-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </div>
-        </div>
-      </div>
+      <h1>This is a main page: {mainPage.data.title}</h1>
+      <article className="prose">
+        <Markdown>{mainPage.content}</Markdown>
+      </article>
     </div>
   );
 };
